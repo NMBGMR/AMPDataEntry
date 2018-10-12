00467ca679e23f8d11682e9100ee8db62921a48e
Jake Ross <jirhiker@gmail.com>
2018-10-12 12:07:36 -0600

    started application skeleton

---
+++ frontend/application.py
@@ -0,0 +1,42 @@
+# ===============================================================================
+# Copyright 2018 ross
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============================================================================
+from traits.api import HasTraits, File, Button, Instance
+from traitsui.api import View, UItem
+
+from backend.parser import Parser
+
+
+class Application(HasTraits):
+
+    source_file = File
+    parse_button = Button('Parse')
+    parser = Instance(Parser)
+
+    def _parse_button_fired(self):
+        self.parser.parse(self.source_file)
+
+    def _parser_default(self):
+        return Parser()
+
+    def traits_view(self):
+        v = View(UItem('source_file'),
+                 UItem('parse_button', enabled_when='source_file'),
+                 title='AMP Data Entry')
+        return v
+
+
+
+# ============= EOF =============================================
