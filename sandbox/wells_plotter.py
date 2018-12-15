e95943bead9aadbdf35970804b2f56b1178b21aa
gabe-parrish <gbrlparrish@gmail.com>
2018-12-14 18:31:59 -0700

    wells plotter formatting more or less complete

--- sandbox/wells_plotter.py
+++ sandbox/wells_plotter.py
@@ -37,18 +37,13 @@ def fix_dates(df):
 
 
 def get_data():
-    data_path = '~/Downloads/magdalena_wells_17_18.csv'
-    waterlevel_path = '~/Downloads/WaterLevelsContinuous_Pressure_mg_038_0717_1118_for_import.csv'
-    precipitation_path = '~/Downloads/magdalena_kellyranch_weather.csv'
+    data_path = 'Z:\data\datasets\magdalena_wells\magdalena_wells_17_18.csv'
+    waterlevel_path = 'Z:\data\datasets\magdalena_wells\WaterLevelsContinuous_Pressure_mg_038_0717_1118_for_import.csv'
+    precipitation_path = 'Z:\data\datasets\magdalena_wells\magdalena_kellyranch_weather.csv'
 
     wd_df = pd.read_csv(data_path)
     wl_df = pd.read_csv(waterlevel_path)
     precip_df = pd.read_csv(precipitation_path)
-
-    # create usage columns with gallons in 10,000 gallon units
-    # wd_df['trujillo_hundredthou'] = wd_df['trujillo_usage'] / 100000.0
-    # wd_df['benjamin_hundredthou'] = wd_df['benjamin_usage'] / 100000.0
-    # print 'data df \n', wd_df
 
     # fix dates
     fix_dates(wl_df)
@@ -80,26 +75,38 @@ def datetimes_to_floats(ds):
 
 
 def plot(wd_df, wl_df, precip_df):
+
+    # ===== Benjamin =====
     fig, (ax1, ax2) = plt.subplots(2)
+    fig.subplots_adjust(hspace=0)
+
+    fig.suptitle('Withdrawals, Precipitation and Waterlevels in Benjamin Well Magdalena, NM', size='20')
+    # plt.style.use('seaborn-notebook')
+    plt.style.use('seaborn-pastel')
 
     wl_xs, wl_ys = get_water_levels(wl_df)
     # plot water levels
-    ax2.plot(wl_xs, wl_ys)
+    ax2.plot(wl_xs, wl_ys, color='b', linewidth=2.0, label='Trujillo Test Well')
     ax2.set_xlabel('Date')
-    ax2.set_ylabel('Depth to Water (ft bgs)')
+    ax2.set_ylabel('Depth to Water (ft bgs)', color='b')
     ax2.invert_yaxis()
 
-    # plot withdraws
+    # plot withdrawals
     wd_xs, wd_ys = get_withdrawals(wd_df, 'benjamin_usage')
     ax2_right = ax2.twinx()
-    ax2_right.plot(wd_xs, wd_ys)
-    ax2_right.set_ylabel('Gallons x 100,000/month withdrawn')
+    ax2_right.plot(wd_xs, wd_ys, color='g', linewidth=3.0, label='Benjamin Well')
+    ax2_right.set_ylabel('Gallons x 100,000/month withdrawn', color='g')
 
     # plot precip
     p_xs, p_ys = get_precip(precip_df)
-    ax1.bar(p_xs, p_ys)
+    ax1.bar(p_xs, p_ys, color='k', label='Magdalena Met. Station')
+    # ax1.yaxis.tick_right()
+    for i, label in enumerate(ax1.get_yticklabels()):
+        if i == 0:
+            label.set_visible(False)
+
     ax1.set_xlabel('Date')
-    ax1.set_ylabel('Precipitation (mm)')
+    ax1.set_ylabel('Precipitation (mm)', color='r')
 
     # # set x limits to the same values
     wl_xs = datetimes_to_floats(wl_xs)
@@ -111,6 +118,58 @@ def plot(wd_df, wl_df, precip_df):
     ax1.set_xlim(min_xs, max_xs)
     ax1.get_xaxis().set_visible(False)
 
+    ax1.legend(loc='upper right', prop={'size': 10})
+    ax2.legend(loc='upper center', prop={'size': 10})
+    ax2_right.legend(loc='lower center', prop={'size': 10})
+
+    plt.show()
+
+    # ===== Trujillo =====
+    fig, (ax1, ax2) = plt.subplots(2)
+    fig.subplots_adjust(hspace=0)
+
+    fig.suptitle('Withdrawals, Precipitation and Waterlevels in Trujillo Well Magdalena, NM', size=20)
+    # plt.style.use('seaborn-notebook')
+
+    wl_xs, wl_ys = get_water_levels(wl_df)
+    # plot water levels
+    tw_test = ax2.plot(wl_xs, wl_ys, color='b', linewidth=2.0, label='Trujillo Test Well')
+    ax2.set_xlabel('Date')
+    ax2.set_ylabel('Depth to Water (ft bgs)', color='b')
+    ax2.invert_yaxis()
+
+    # plot withdrawals
+    wd_xs, wd_ys = get_withdrawals(wd_df, 'trujillo_usage')
+    ax2_right = ax2.twinx()
+    tw = ax2_right.plot(wd_xs, wd_ys, color='g', linewidth=3.0, label='Trujillo Well')
+    ax2_right.set_ylabel('Gallons x 100,000/month withdrawn', color='g')
+
+    # plot precip
+    p_xs, p_ys = get_precip(precip_df)
+    mag_met = ax1.bar(p_xs, p_ys, color='black', label='Magdalena Met. Station')
+    # ax1.yaxis.tick_right()
+    for i, label in enumerate(ax1.get_yticklabels()):
+        if i == 0:
+            label.set_visible(False)
+
+    ax1.set_xlabel('Date')
+    ax1.set_ylabel('Precipitation (mm)', color='r')
+
+    # # set x limits to the same values
+    wl_xs = datetimes_to_floats(wl_xs)
+    wd_xs = datetimes_to_floats(wd_xs)
+
+    min_xs = min(min(wl_xs), min(wd_xs), min(p_xs))
+    max_xs = max(max(wl_xs), max(wd_xs), max(p_xs))
+
+    ax1.set_xlim(min_xs, max_xs)
+    ax1.get_xaxis().set_visible(False)
+
+    ax1.legend(loc='upper right', prop={'size':10})
+    ax2.legend(loc='lower center', prop={'size':10})
+    ax2_right.legend(loc='upper center', prop={'size':10})
+    # fig.legend((tw_test, tw, mag_met), ('Magdalena Met. Station', 'Trujillo Test Well', 'Trujillo Well'), 'upper right')
+    # plt.tight_layout()
     plt.show()
 
 
